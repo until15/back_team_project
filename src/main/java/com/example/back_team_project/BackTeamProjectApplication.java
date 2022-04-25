61f637ba0e867d604258a4a0dbc8925007ddb2a1
akaksk <kangs6n@gmail.com>
2022-04-25 10:55:56 +0900

    폼 수정

--- src/main/java/com/example/back_team_project/BackTeamProjectApplication.java
+++ src/main/java/com/example/back_team_project/BackTeamProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 
@@ -24,9 +25,9 @@ import org.springframework.context.annotation.ComponentScan;
 })
 
 // 저장소(jpa) 
-// @EnableJpaRepositories(basePackages = {
-// 	"com.example.repository"
-// })
+@EnableJpaRepositories(basePackages = {
+ 	"com.example.repository"
+})
 
 // filter
 @ServletComponentScan(basePackages = {
