e20f73e2dfbfb5c130e3510c110612e9f9281ada
akaksk <kangs6n@gmail.com>
2022-04-25 17:27:59 +0900

    service

---
+++ src/main/java/com/example/service/ChallengeService.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.entity.ChallengeCHG;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ChallengeService {
+    
+    // 챌린지 등록
+    public int insertChallengeOne(ChallengeCHG challenge);
+
+    // 챌린지 1개 조회
+    public ChallengeCHG RoutineSelectOne(long challenge);
+
+    // 챌린지 목록 (페이지 1 2 3)
+    public List<ChallengeCHG> MemberSelectList(Pageable page, String challenge);
+
+    // 챌린지 수정
+    public int challengeUpdateOne(ChallengeCHG challenge);
+
+    // 챌린지 삭제
+    public ChallengeCHG deleteChallenge(long challenge);
+
+    // 챌린지 시작
+    public int challengeStart(ChallengeCHG challenge);
+
+    // 챌린지 종료
+    public int challengeEnd(ChallengeCHG challenge);
+
+    // 모집 시작일 
+    public int recruitStart(ChallengeCHG challenge);
+
+    // 모집 마감일
+    public int recruitEnd(ChallengeCHG challenge);
+
+
+}
