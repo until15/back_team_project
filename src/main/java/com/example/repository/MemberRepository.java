a1d882034eb4661eb1e0f412732b38b7a2854c75
daheeka <alsendrha@naver.com>
2022-04-25 12:36:57 +0900

    444
    4

--- src/main/java/com/example/repository/MemberRepository.java
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -14,11 +14,6 @@ public interface MemberRepository extends JpaRepository<MemberCHG, String> {
     long countByMemailContaining(String memail);
 
     // findBy 컬럼명 ContainingOrderBy컬럼명Desc
-    // SELECT * FROM 테이블명
-    // WHERE BTITLE LIKE '%' || '검색어' || '%' ORDER BY NO DESC
-    List<MemberCHG> findByMemailContainingOrderByNoDesc(String memail);
-
-    // findBy 컬럼명 ContainingOrderBy컬럼명Desc
     // SELECT B.*, ROW_NUMBER() OVER (ORDER BY DESC) FROM BOARD10 B
     // WHERE BTITLE LIKE '%' || '검색어' || '%'
     List<MemberCHG> findByMemailContainingOrderByNoDesc(String Memail, Pageable page);
