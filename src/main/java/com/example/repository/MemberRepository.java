10ef9b6b20dc3a786d4dea22520b116b523454aa
daheeka <alsendrha@naver.com>
2022-04-25 12:36:20 +0900

    3333
    3

--- src/main/java/com/example/repository/MemberRepository.java
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.example.entity.MemberCHG;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,5 +17,10 @@ public interface MemberRepository extends JpaRepository<MemberCHG, String> {
     // SELECT * FROM 테이블명
     // WHERE BTITLE LIKE '%' || '검색어' || '%' ORDER BY NO DESC
     List<MemberCHG> findByMemailContainingOrderByNoDesc(String memail);
+
+    // findBy 컬럼명 ContainingOrderBy컬럼명Desc
+    // SELECT B.*, ROW_NUMBER() OVER (ORDER BY DESC) FROM BOARD10 B
+    // WHERE BTITLE LIKE '%' || '검색어' || '%'
+    List<MemberCHG> findByMemailContainingOrderByNoDesc(String Memail, Pageable page);
 
 }
