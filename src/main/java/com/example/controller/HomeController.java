dd5d1c1b474d95ed5fc0f76ea1b0f2246c74ff5b
daheeka <alsendrha@naver.com>
2022-04-26 11:32:57 +0900

    887
    8787

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
-    @GetMapping(value = {"/", "/home"})
+
+    @GetMapping(value = { "/", "/home" })
     public String HomeGET() {
+
         return "home";
     }
 }
-
