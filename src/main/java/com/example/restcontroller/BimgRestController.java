660205176cc59bd280c94748759dbdf6518cb630
akaksk <kangs6n@gmail.com>
2022-05-05 22:54:13 +0900

    1
    1

--- src/main/java/com/example/restcontroller/BimgRestController.java
+++ src/main/java/com/example/restcontroller/BimgRestController.java
@@ -65,6 +65,7 @@ public class BimgRestController {
             BimgCHG bimg = new BimgCHG();
             CommunityCHG community = new CommunityCHG();
             community.setBno(bno);
+            c
             bimg.setCommunitychg(community);
             String username = jwtUtil.extractUsername(token);
             System.out.println(username);
