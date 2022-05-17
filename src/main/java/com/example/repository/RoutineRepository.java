c088cc7187dc90ec323b5c1eff75bb912dd3639c
daheeka <rudtjd98@naver.com>
2022-05-17 12:20:24 +0900

    RoutineUpdate

--- src/main/java/com/example/repository/RoutineRepository.java
+++ src/main/java/com/example/repository/RoutineRepository.java
@@ -2,13 +2,15 @@ package com.example.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.example.entity.RoutineCHG;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoutineRepository extends JpaRepository<RoutineCHG, Long>{
+public interface RoutineRepository extends JpaRepository<RoutineCHG, Long> {
 
     // 루틴 조회
     List<RoutineCHG> findByMemberchg_memailEqualsOrderByRtnnoDesc(String memail);
@@ -16,5 +18,8 @@ public interface RoutineRepository extends JpaRepository<RoutineCHG, Long>{
     List<RoutineCHG> findByRtnnoIn(List<RoutineCHG> rtnnos);
 
     List<RoutineCHG> findByMemberchg_memailAndRtnseq(String memail, long rtnseq);
-    
+
+    @Transactional
+    int deleteByMemberchg_memailAndRtnnoIn(String memail, Long[] rtnno);
+
 }
