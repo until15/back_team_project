35c14fb6c2671c464373069ee2593ca47d08bb73
dlxorb96 <blesty5683@naver.com>
2022-05-18 17:42:08 +0900

    confirm image
    인증 조회, 인증 이미지 불러오기

--- src/main/java/com/example/repository/CfImageRepository.java
+++ src/main/java/com/example/repository/CfImageRepository.java
@@ -1,11 +1,22 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.CfImageCHG;
 
+// 인증 이미지 저장소
 @Repository
 public interface CfImageRepository extends JpaRepository<CfImageCHG, Long> {
 
+	public CfImageCHG findByCfimgno(long cfino);
+	
+	@Query(value = "SELECT CFI.CFIMGNO  FROM CF_IMAGECHG CFI WHERE CFI.CFNO=:no", nativeQuery = true)
+	public List<Long> selectCFImageNo(@Param("no") long cfno);
+	
+	
 }
