7a2da90232be3feb2482d22fe092034c93c7f5cf
daheeka <alsendrha@naver.com>
2022-05-18 09:16:30 +0900

    34534534
    erter

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-
 import java.io.IOException;
 
 import com.example.entity.MemberCHG;
@@ -22,9 +21,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping(value = "/member")
 public class MemberController {
-    
-    @Autowired MemberService mService;
-    
+
+    @Autowired
+    MemberService mService;
+
     // 로그인
     // 127.0.0.1:9090/ROOT/member/login
     @GetMapping(value = "/login")
@@ -42,28 +42,27 @@ public class MemberController {
     // 회원가입 동작, 프로필 사진
     // 127.0.0.1:9090/ROOT/member/joinaction
     @PostMapping(value = "/joinaction")
-    public String joinPOST( 
-        @ModelAttribute MemberCHG member,
-        @AuthenticationPrincipal User user,                // userDetailsService 
-        @RequestParam(name = "mimage") MultipartFile file  // 이미지
+    public String joinPOST(
+            @ModelAttribute MemberCHG member,
+            @AuthenticationPrincipal User user, // userDetailsService
+            @RequestParam(name = "mimage") MultipartFile file // 이미지
     ) throws IOException {
         BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
         System.out.println(bcpe.toString()); // 확인
 
-        member.setMpw( bcpe.encode( member.getMpw() ) );  // 비밀번호 설정
-        member.setMrole(member.getMrole());               // 권한 설정
+        member.setMpw(bcpe.encode(member.getMpw())); // 비밀번호 설정
+        member.setMrole(member.getMrole()); // 권한 설정
 
         // 프로필 이미지
         System.out.println(file.getOriginalFilename());
-        member.setMprofile(file.getBytes());          // 프로필 이미지
+        member.setMprofile(file.getBytes()); // 프로필 이미지
         member.setMpname(file.getOriginalFilename()); // 이미지 이름
-        member.setMpsize(file.getSize());             // 이미지 사이즈
-        member.setMptype(file.getContentType());      // 이미지 타입
+        member.setMpsize(file.getSize()); // 이미지 사이즈
+        member.setMptype(file.getContentType()); // 이미지 타입
 
-        mService.MemberInsertOne(member);
-        
+        mService.memberInsertOne(member);
+
         return "redirect:/member/login";
-    }    
-
+    }
 
 }
