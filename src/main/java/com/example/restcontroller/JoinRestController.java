ecb8a6a0104d48a605580dfa26a3f61e2ccab4ac
kimminju <blesty5683@naver.com>
2022-06-16 20:24:56 +0900

    delete syso
    system.out.println 제거

--- src/main/java/com/example/restcontroller/JoinRestController.java
+++ src/main/java/com/example/restcontroller/JoinRestController.java
@@ -182,21 +182,17 @@ public class JoinRestController {
 	// 127.0.0.1:9090/until15/api/join/insert?chgno=
 	// Headers : token
 	@RequestMapping(value = "/insert", 
-			method = { RequestMethod.POST }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+					method = { RequestMethod.POST },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> insertJoinPOST(
 			@RequestHeader(name = "token") String token,
 			@RequestParam(name = "chgno") long chgno) {
 
 		Map<String, Object> map = new HashMap<>();
 		try {
-			System.out.println("토큰 발급 : " + token); // 토큰 발급
-			System.out.println(chgno); // 넘어오는 join
-
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-			System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 변환
 			JSONObject jsonObject = new JSONObject(userSubject);
@@ -204,36 +200,24 @@ public class JoinRestController {
 			String email = jsonObject.getString("username"); // 회원 아이디
 			long rank = jsonObject.getLong("userrank"); // 회원 등급
 
-			System.out.println(email);
-			System.out.println(rank);
-
 			// member 엔티티에 아이디 담기
 			MemberCHG memail = new MemberCHG();
 			memail.setMemail(email);
 
-			// System.out.println("아이디 : " + memail);
-
 			// 등급을 조회하기 위해 첼린지 번호로 조회
 			ChallengeProjection chg = chgRepository.findByChgno(chgno);
-			// System.out.println("첼린지 번호로 난이도 조회 : " + chg.getChglevel().toString());
 
 			// challenge 엔티티에 첼린지 번호 담기
 			ChallengeCHG challenge = new ChallengeCHG();
 			challenge.setChgno(chgno);
-
-			// System.out.println(challenge);
 
 			// 참가 엔티티에 아이디와 첼린지 번호 담기
 			JoinCHG join = new JoinCHG();
 			join.setMemberchg(memail);
 			join.setChallengechg(challenge);
 
-			// System.out.println(join); // join엔티티에 담겨있는 값 확인
-
 			// 아이디와 첼린지 번호 동시에 일치하는 지 확인
 			JoinCHG duplicate = jService.duplicateJoin(chgno, email);
-			// System.out.println(duplicate);
-			
 
 			// 기존에 가입한 첼린지가 아닐 때 참가 가능
 			if (duplicate == null) {
@@ -242,13 +226,10 @@ public class JoinRestController {
 
 					int ret = jService.challengeJoin(join);
 					if (ret == 1) {
-						
 						JoinOneView join1 = joRepository.findByMemailAndChgno(email, chgno);
-						System.out.println(join1.getJno());
 						
 						// 참가할 때마다 첼린지 인원수 1씩 증가
-						int ret1 = chgRepository.increaseCnt(chgno);
-						System.out.println(ret1);
+						chgRepository.increaseCnt(chgno);
 
 						// 새로 참가하기는 200
 						map.put("result", join1.getJno());
@@ -274,30 +255,26 @@ public class JoinRestController {
 	// 포기하기 (chgstate 가 1이면 참여중 2면 포기)
 	// 127.0.0.1:9090/until15/api/join/giveup?chgno=
 	// Headers : token
-	@RequestMapping(value = "/giveup", method = { RequestMethod.PUT }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/giveup",
+					method = { RequestMethod.PUT },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> giveupCHG(
 			@RequestHeader(name = "token") String token,
 			@RequestParam(name = "chgno") long chgno) {
 
 		Map<String, Object> map = new HashMap<>();
 		try {
-			System.out.println("토큰 발급 : " + token); // 토큰 발급
-			System.out.println(chgno); // 넘어오는 join
-
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-			System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 파싱
 			JSONObject jsonObject = new JSONObject(userSubject);
 			String email = jsonObject.getString("username");
 
-			System.out.println(email);
-
 			JoinCHG join = jService.duplicateJoin(chgno, email);
 
+			// chgstate의 값이 2 = 포기
 			join.setChgstate(2);
 
 			int ret = jService.challengeGiveUp(join);
@@ -318,9 +295,10 @@ public class JoinRestController {
 
 	// 진행 상태에 따른 조회 리스트
 	// 127.0.0.1:9090/until15/api/join/joinstate?chgstate=&page=
-	@RequestMapping(value = "/joinstate", method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/joinstate",
+					method = { RequestMethod.GET },
+					consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> joinStateListGET(
 			@RequestHeader(name = "token") String token,
 			@RequestParam(name = "chgstate", defaultValue = "1") int state,
@@ -328,30 +306,21 @@ public class JoinRestController {
 
 		Map<String, Object> map = new HashMap<>();
 		try {
-			// System.out.println(state);
-			// System.out.println(token);
-			// System.out.println(page);
-
 			// 페이지네이션
 			PageRequest pageRequest = PageRequest.of(page - 1, 10);
-			// System.out.println(pageRequest);
 
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-			// System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 파싱
 			JSONObject jsonObject = new JSONObject(userSubject);
 			String email = jsonObject.getString("username");
 
-			// System.out.println(email);
-
 			// 아이디, 진행 상태, 페이지네이션으로 조회
 			List<JoinProjection> list = jService.joinStateList(email, state, pageRequest);
-			// System.out.println(list);
 
+			// 전체 갯수
 			long total = jService.selectStateCount(email, state);
-			// System.out.println(total);
 
 			if (!list.isEmpty()) {
 				map.put("pages", total);
@@ -373,36 +342,29 @@ public class JoinRestController {
 	// 내가 만든 첼린지 리스트 조회 (페이지네이션)
 	// 127.0.0.1:9090/until15/api/join/cidselectlist?page=&text=
 	@RequestMapping(value = "/cidselectlist", 
-			method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+					method = { RequestMethod.GET },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> cIdSelectListGET(
 			@RequestParam(name = "page", defaultValue = "1") int page,
 			@RequestParam(name = "text", defaultValue = "") String text,
 			@RequestHeader(name = "token") String token ){
 		Map<String, Object> map = new HashMap<>();
 		try {
-//			System.out.println("페이지 : " + page);
-//			System.out.println("토큰 : " + token);
-			
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-//			System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 파싱
 			JSONObject jsonObject = new JSONObject(userSubject);
 			String email = jsonObject.getString("username");
-
-//			System.out.println(email);
 			
 			// 페이지네이션(시작페이지(0부터), 갯수)
 			PageRequest pageRequest = PageRequest.of(page - 1, 5);
-//			System.out.println("페이지네이션 : " + pageRequest);
 			
 			List<ChallengeProjection> list = chgRepository.findByMemberchg_memailAndChgtitleContaining(email, text, pageRequest);
 			
 			long total = chgRepository.countByMemberchg_memailAndChgtitleContaining(email, text);
-//			System.out.println("항목 갯수 : " + total);
+
 			if (!list.equals(null)) {
 				map.put("pages", total);
 				map.put("result", list);
@@ -422,8 +384,8 @@ public class JoinRestController {
 	// 내가 생성한 첼린지 상세 조회
 	// 127.0.0.1:9090/until15/api/join/cidselectone?chgno=81
 	@RequestMapping(value = "/cidselectone", 
-			method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
+			method = { RequestMethod.GET },
+			consumes = { MediaType.ALL_VALUE },
 			produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> cIdSelectOneGET(
 			@RequestParam(name = "chgno") long chgno,
@@ -466,37 +428,25 @@ public class JoinRestController {
 	
 	// 내가 참여한 첼린지 1개 상세 조회
 	// 127.0.0.1:9090/until15/api/join/selectone?jno=8
-	@RequestMapping(value = "/selectone", method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/selectone",
+					method = { RequestMethod.GET },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> selectOneGET(
 			@RequestParam(name = "jno") long jno,
 			@RequestHeader(name = "token") String token) {
 		Map<String, Object> map = new HashMap<>();
 		try {
-
-			System.out.println(jno);
-			System.out.println(token);
-
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-			System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 파싱
 			JSONObject jsonObject = new JSONObject(userSubject);
 			String email = jsonObject.getString("username");
 
-			System.out.println(email);
-
-			// 아이디와 번호가 동시에 일치하는 것 조회
-//			JoinSelectOne join = jService.selectOneCHG(email, jno);
-//			System.out.println(join);
-
 			JoinOneView join = joRepository.findByMemailAndJno(email, jno);
 
 			String thumbnail = "/until15/api/join/thumbnail?chgno="+join.getChgno();
-
-			System.out.println(join.getJregdate());
 
 			if (!join.equals(null)) {
 				map.put("image", thumbnail);
@@ -517,32 +467,24 @@ public class JoinRestController {
 	// 진행 중인 내 첼린지 전체 조회
 	// 127.0.0.1:9090/until15/api/join/inglist
 	// Headers => token :
-	@RequestMapping(value = "/inglist", method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/inglist",
+					method = { RequestMethod.GET },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Map<String, Object> selectIngListGET(
 			@RequestHeader(name = "token") String token) {
 
 		Map<String, Object> map = new HashMap<>();
 		try {
-			// System.out.println(token);
-
 			// 토큰에서 정보 추출
 			String userSubject = jwtUtil.extractUsername(token);
-			// System.out.println("토큰에 담긴 전보 : " + userSubject);
 
 			// 추출된 결과값을 JSONObject 형태로 파싱
 			JSONObject jsonObject = new JSONObject(userSubject);
 			String email = jsonObject.getString("username");
 
-			System.out.println(email);
-
 			// 참가상태 변수 : 1 => 진행중
 			int state = 1;
-
-			// 아이디와 참가 변수를 전달해서 진행 중인 첼린지만 조회
-			// List<JoinProjection> list = jService.joinChallengeList(email, state);
-			// System.out.println(list);
 
 			// 진행 중인 첼린지 썸네일 포함 9개만 조회
 			List<JoinCHGView> list = jingRepository.selectJoinningCHG(email, state);
@@ -553,7 +495,6 @@ public class JoinRestController {
 				imgs[i] = "/until15/api/join/thumbnail?chgno=" + list.get(i).getChgno();
 				list.get(i).setJimgurl(imgs[i]);
 			}
-			System.out.println("이미지 url : " + imgs.toString());
 
 			map.put("images", imgs);
 			map.put("result", list);
@@ -571,16 +512,16 @@ public class JoinRestController {
 
 	// 썸네일 조회
 	// 127.0.0.1:9090/until15/api/join/thumbnail?chgno=
-	@RequestMapping(value = "/thumbnail", method = { RequestMethod.GET }, // POST로 받음
-			consumes = { MediaType.ALL_VALUE }, // 모든 타입을 다 받음
-			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "/thumbnail",
+					method = { RequestMethod.GET },
+					consumes = { MediaType.ALL_VALUE },
+					produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<byte[]> selectImageGET(
 			@RequestParam(name = "chgno") long chgno) {
 		try {
 			// 이미지를 한개 조회
 			CHGImgView chgImage = chgIRepository.findByChgno(chgno);
 
-			System.out.println("이미지 조회 : " + chgImage.getChgisize());
 			// 썸네일 이미지가 있을 때
 			if (chgImage.getChgisize() > 0) {
 				HttpHeaders header = new HttpHeaders();
